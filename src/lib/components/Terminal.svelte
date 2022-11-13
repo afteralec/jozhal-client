2bf406960d252437b07c7c733420cf9bc8613ffd
Alec DuBois <after.alec@gmail.com>
2022-11-12 23:03:12 -0800

    Prototype Opening and Closing Prompt Scope
    Prototypes some handling that opens and closes a prompt scope - next is
    to capture and process the internal matter.

--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -36,15 +36,7 @@
     const unlisten = await listen(
       "armageddon_output",
       (e: Event<Uint8Array>) => {
-        const payloadString = String.fromCharCode(...e.payload);
-        if (payloadString.includes("__begin_prompt")) {
-          isPrompt = true;
-          console.log("prompt begins caught: " + payloadString);
-        }
-        if (payloadString.includes("__end_prompt")) {
-          isPrompt = false;
-          console.log("prompt ends caught: " + payloadString);
-        }
+        processPrompt(e.payload);
 
         terminal.write(e.payload);
       }
@@ -57,6 +49,103 @@
       unlisten();
     };
   });
+
+  function processPrompt(bytes: Uint8Array) {
+    for (let i = 0; i < bytes.length; i++) {
+      if (bytes.includes(95)) {
+        if (payloadContainsBeginPrompt(bytes)) {
+          const payloadString = String.fromCharCode(...bytes);
+          const promptMatter = payloadString.split("__begin_prompt");
+          console.log("caught begin prompt: " + promptMatter);
+          isPrompt = true;
+
+          if (payloadContainsEndPrompt(bytes)) {
+            console.log("caught end of prompt in same payload as begin prompt");
+            isPrompt = false;
+            return;
+          }
+        } else if (isPrompt) {
+          if (payloadContainsEndPrompt(bytes)) {
+            const payloadString = String.fromCharCode(...bytes);
+            const promptMatter = payloadString.split("__begin_prompt");
+            console.log("caught end prompt: " + promptMatter);
+            isPrompt = false;
+            return;
+          } else {
+            const payloadString = String.fromCharCode(...bytes);
+            const promptMatter = payloadString.split("__begin_prompt");
+            console.log("middle prompt matter: " + promptMatter);
+          }
+        }
+      }
+    }
+  }
+
+  function payloadContainsBeginPrompt(bytes: Uint8Array) {
+    for (let i = 0; i < bytes.length; i++) {
+      const byte = bytes[i];
+
+      if (byte === 95) {
+        if (payloadContainsBeginPromptFromStart(bytes, i)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  let beginPromptBytes = [
+    95, 95, 98, 101, 103, 105, 110, 95, 112, 114, 111, 109, 112, 116
+  ];
+  function payloadContainsBeginPromptFromStart(
+    bytes: Uint8Array,
+    startIndex: number
+  ) {
+    let j = 1;
+    for (let i = startIndex; i < startIndex + 13; i++) {
+      if (!bytes[i]) return false;
+
+      if (bytes[i] !== beginPromptBytes[j]) return false;
+
+      j++;
+    }
+
+    return true;
+  }
+
+  function payloadContainsEndPrompt(bytes: Uint8Array) {
+    for (let i = 0; i < bytes.length; i++) {
+      const byte = bytes[i];
+
+      if (byte === 95) {
+        if (payloadContainsEndPromptFromStart(bytes, i)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  let endPromptBytes = [
+    95, 95, 101, 110, 100, 95, 112, 114, 111, 109, 112, 116
+  ];
+  function payloadContainsEndPromptFromStart(
+    bytes: Uint8Array,
+    startIndex: number
+  ) {
+    let j = 1;
+    for (let i = startIndex; i < startIndex + 11; i++) {
+      if (!bytes[i]) return false;
+
+      if (bytes[i] !== endPromptBytes[j]) return false;
+
+      j++;
+    }
+
+    return true;
+  }
 </script>
 
 <div id="terminal" class="terminal-height" />
